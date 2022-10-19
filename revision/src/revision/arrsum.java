6df81310e73d60eb56c5af1eb8db21ef25ccda6e
Nivya-235693 <112929554+Nivya-235693@users.noreply.github.com>
2022-10-19 17:51:29 +0530

    change

---
+++ revision/src/revision/arrsum.java
@@ -0,0 +1,30 @@
+package revision;
+import java.util.Scanner;
+
+public class arrsum {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int arr[]=new int[20];
+		int n,s=0;
+		System.out.println("Enter number of elements : ");
+		n=sc.nextInt();
+		if(n==0 || n<0) {
+			System.out.println("Invalid Range");
+			System.exit(0);
+		}
+		else {
+			System.out.println("Enter the elements");
+			for(int i=0;i<n;i++) {
+				arr[i]=sc.nextInt();
+		}
+			for(int i=0;i<n;i++) {
+				s=arr[i]+s;
+				System.out.print(s+" ");
+			}
+		}
+		
+
+	}
+
+}
