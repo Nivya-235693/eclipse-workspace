045446ccef57f68a3b0e154a9a7cf9fb6c67eee4
Nivya-235693 <112929554+Nivya-235693@users.noreply.github.com>
2022-10-21 15:49:15 +0530

    revision

---
+++ revision/src/revision/VisitirsDetails.java
@@ -0,0 +1,101 @@
+package revision;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+class FileManager{
+    static public File createFile()
+    {
+        File file = new File("visitordetails.txt");
+        try
+        {
+            if(file.createNewFile())
+                System.out.println("File created successfully");
+            
+        }
+        catch(IOException ie){
+            System.out.println("File already exists");
+        }
+        return file;
+        
+    }
+    static public void writeFile(File f,String record)
+    {
+        try {
+            FileWriter fw = new FileWriter(f,true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.append(record);
+            System.out.println("Successfully written to file");
+            bw.close();
+            fw.close();
+        }
+        catch(IOException ie){
+            System.out.println(ie);
+        }
+        
+    }
+    static public String[] readFile(File f)
+    {
+        ArrayList<String> arr = new ArrayList<String>();
+        try {
+            Scanner sc = new Scanner(f);
+            sc.useDelimiter(";");
+            while(sc.hasNext())
+                    arr.add(sc.next());
+            String[] fileContent = new String[arr.size()];
+            for(int i=0;i<arr.size();i++)
+                    fileContent[i] = arr.get(i);
+            sc.close();
+            return fileContent;
+            
+                
+        }
+        catch(IOException ie)
+        {
+            System.out.println(ie);
+        }
+        return null;
+        
+        
+    }
+}
+
+
+public class VisitirsDetails {
+
+
+
+	   public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        char ch;
+	        File file = FileManager.createFile();
+	        do {
+	            String ip = "";
+	            System.out.println("Enter Name");
+	            //sc.nextLine();
+	            String name = sc.next();
+	            ip += name;
+	            System.out.println("Enter Phone Number");
+	            String phno = sc.next();
+	            ip += ","+phno;
+	            System.out.println("Enter email");
+	            String email = sc.next();
+	            ip += ","+email+";";            
+	            FileManager.writeFile(file,ip);
+	            System.out.println("Do you want to enter another record?(y/n)");
+	            ch = sc.next().charAt(0);            
+	        }while(ch == 'y');
+	        String[] fileContent = FileManager.readFile(file);
+	        for(String str:fileContent)
+	            System.out.println(str);
+	        
+	        
+	   }
+}
